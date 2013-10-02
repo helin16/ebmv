57487ce3585255fe00926189fc84e1e80102d491
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-02 00:54:45 +0000

    created product and category service

---
+++ core/main/service/CategoryService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Category service
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class CategoryService extends BaseService
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("Category");
+    }
+}
+?>
---
+++ core/main/service/ProductService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Product service
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class ProductService extends BaseService
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("Product");
+    }
+}
+?>
