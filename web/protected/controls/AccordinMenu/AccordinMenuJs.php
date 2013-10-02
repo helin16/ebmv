675451b4a4f47357294642d05561324ba9b1617a
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-02 06:03:44 +0000

    added accordin menu JS

---
+++ web/protected/controls/AccordinMenu/AccordinMenuJs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The AccordinMenu Loader
+ * @author lhe
+ *
+ */
+class AccordinMenuJs extends TClientScript
+{
+    /**
+     * (non-PHPdoc)
+     * @see TPage::render()
+     */
+    public function onLoad($param)
+    {
+        parent::onLoad($param);
+        $this->getPage()->getClientScript()->registerScriptFile('accordinJs', $this->publishAsset('ddsmoothmenu.js'));
+        $this->getPage()->getClientScript()->registerStyleSheetFile('accordinCss', $this->publishAsset('ddsmoothmenu.css'));
+        $this->getPage()->getClientScript()->registerStyleSheetFile('accordinCssV', $this->publishAsset('ddsmoothmenu-v.css'));
+        $this->publishAsset('down_over.gif');
+        $this->publishAsset('down.gif');
+        $this->publishAsset('right_over.gif');
+        $this->publishAsset('right.gif');
+    }
+}
