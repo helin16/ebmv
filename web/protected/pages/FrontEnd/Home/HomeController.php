33c34540846ac6c943f0e8f0151d5f34a5639024
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-02 06:42:39 +0000

    added JQuery

--- web/protected/pages/FrontEnd/Home/HomeController.php
+++ web/protected/pages/FrontEnd/Home/HomeController.php
@@ -6,7 +6,7 @@
  * @subpackage Controller
  * @author     lhe<helin16@gmail.com>
  */
-class HomeController extends TPage  
+class HomeController extends FrontEndPageAbstract  
 {
     /**
      * (non-PHPdoc)
@@ -14,6 +14,67 @@ class HomeController extends TPage
      */
 	public function onLoad($param)
 	{
+	    $this->getClientScript()->registerEndScript('navListJs', $this->_getNavListJs());
+	}
+	
+	private function _getNavListJs()
+	{
+	    return 'ddsmoothmenu.init({
+             mainmenuid: "catelist",
+             orientation: "v",
+             classname: "ddsmoothmenu-v",
+             arrowswap: true,
+             contentsource: "markup"});';
+	}
+	
+	public function getCategories()
+	{
+	    $categories = array();
+	    $relArray = $this->_getCategoryArray($categories);
+	    return $this->_getCateLi($relArray, $relArray, $categories);
+	}
+	
+	private function _getCateLi($array, $relArray, &$categories, $htmlId = '')
+	{
+	    if(!is_array($array) || count($array) === 0)
+	        return;
+	    $html = '<ul>';
+	    foreach($array as $key => $nextLevelArray)
+	    {
+	        if(!isset($categories[$key]))
+	            continue;
+	        $html .= '<li><a href="#">' . $categories[$key]->getName() . '</a>';
+	            unset($categories[$key]);
+	        if(isset($relArray[$key]))
+	            $html .= $this->_getCateLi($relArray[$key], $relArray, $categories);
+	        else
+	            $html .= $this->_getCateLi($nextLevelArray, $relArray, $categories);
+	        $html .= '</li>';
+	    }
+	    $html .= "</ul>";
+	    return $html;
+	}
+	
+	private function _getCategoryArray(&$flatArray = array())
+	{
+	    $array = array();
+	    $flatArray = array();
+	    foreach(BaseService::getInstance('Category')->findAll() as $cate)
+	    {
+	        $flatArray[$cate->getId()] = $cate;
+	        if(!$cate->getParent() instanceof Category)
+	        {
+	            $array[$cate->getId()] = array();
+	            continue;
+	        }
+	        
+	        $parentId = $cate->getParent()->getId();
+	        if(!isset($array[$parentId]))
+	            $array[$parentId] = array();
+	        
+	        $array[$parentId][$cate->getId()] = $cate->getId();
+	    }
+	    return $array;
 	}
 }
 ?>
