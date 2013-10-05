018e2a34a530a9a12e68dd088fc651f0a3d6c20b
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-05 05:57:52 +0000

    added UTF8 to mysql connection

--- core/main/db/Dao.php
+++ core/main/db/Dao.php
@@ -58,7 +58,7 @@ abstract class Dao
             // DSN FORMAT: "mysql:host=localhost;dbname=test"
             $dsn = Config::get('Database', 'Driver') . ':host=' . Config::get('Database', 'LoadBalancer') . ';dbname=' . Config::get('Database', 'CoreDatabase');
             self::$_db = new PDO($dsn, Config::get('Database','Username'), Config::get('Database','Password'));
-            self::$_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }
         catch (PDOException $e)
         {
