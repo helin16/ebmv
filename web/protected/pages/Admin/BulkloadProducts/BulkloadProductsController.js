1b82d695fbd6ed1a20634e8ab22e766fff9c8977
murahman2008@gmail.com <murahman2008@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-11 01:56:18 +0000

    adding the Bulkload instructions and template download options

--- web/protected/pages/Admin/BulkloadProducts/BulkloadProductsController.js
+++ web/protected/pages/Admin/BulkloadProducts/BulkloadProductsController.js
@@ -4,4 +4,27 @@
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new AdminPageJs(), {
 	
+	generateTemplate: function(button, loadLabel) {
+		var tmp = {};
+		tmp.me = this;
+		
+		tmp.me.postAjax(tmp.me.getCallbackId('downloadtemplate'), {}, {
+			'onLoading': function (sender, param) {
+				$(button).hide();
+				$(loadLabel).show()
+			},
+			'onComplete': function (sender, param) {
+				try {
+					tmp.result = pageJs.getResp(param, false, true);
+					if(tmp.result.fileName !== undefined && tmp.result.fileName !== null) {
+						window.open(tmp.result.fileName);
+					}
+				} catch(e) {
+					alert(e);
+				}
+				$(button).show();
+				$(loadLabel).hide();
+			}
+		});
+	}	
 });
--- web/protected/pages/Admin/BulkloadProducts/BulkloadProductsController.php
+++ web/protected/pages/Admin/BulkloadProducts/BulkloadProductsController.php
@@ -1,5 +1,84 @@
 <?php
 class BulkloadProductsController extends AdminPageAbstract
 {
-    
+	public function __construct()
+	{
+		parent::__construct();    
+	}
+	
+	public function onPreInit($param)
+	{
+		parent::onPreInit($param);
+	}
+	
+	public function onInit($param)
+	{
+		parent::onInit($param);
+	}
+	
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+		if(!$this->IsPostBack || $param == "reload")
+		{
+			$this->showInstruction();		
+		}
+	}
+	
+	private function _getInstructionDetails()
+	{
+		return array("Title" => "<b>Mandatory</b> The title of the product", 
+					 "info2" => "The info 2 of the product",
+					 "info3" => "The info 2 of the product",
+					 "info4" => "The info 2 of the product"
+					);
+	}
+	
+	public function generateTemplate($sender, $param)
+	{
+		$content = implode(",", array_keys($this->_getInstructionDetails()));
+		$fileName = "TEMPLATE_FOR_PRODUCT_BULKLOAD";
+		$fileName = $fileName."_".md5($content).".csv";
+		
+		$fullPath = $this->getApplication()->getAssetManager()->getBasePath() . '/' . $fileName;
+		$errors = $return = array();
+		try
+		{
+			if(!file_exists($fullPath))
+				file_put_contents($fullPath, $content);
+				
+//			chmod($fullPath, 0777);
+			$return = array('fileName' => $this->getApplication()->getAssetManager()->getBaseUrl() . '/' . $fileName);
+		}
+		catch(Exception $ex)
+		{
+			$errors[] = $ex->getMessage();
+		}
+		
+		$param->ResponseData = StringUtilsAbstract::getJson($return, $errors);
+	}
+	
+	public function showInstruction()
+	{
+		$instructions = "<div>";
+		$instructions.= "<table style='border: 5px #aaaaaa solid;width:100%; font-size:13px;' cellspacing='0' cellpadding='0'>
+    						<tr>
+    							<td style='background-color:#aaaaaa;padding:5px;'>Sample File filling instruction: </td>
+    						</tr>
+    						<tr>
+    							<td>
+    								<table width='100%'>
+    									<tr style='height:23px;background:#cccccc;'>
+    										<td width='5%'>Col. No.</td>
+    										<td width='10%'>Column Name</td>
+    										<td width='50%'>Description</td>
+    									<tr>";
+		$columnArray = $this->_getInstructionDetails();
+		$rc = 0;
+		foreach($columnArray as $key => $value)
+			$instructions .= "<tr ".($rc % 2 === 0 ? "style='background-color:white;'" : "style='background-color:#cccccc;'")."><td>".(++$rc)."</td><td>".$key."</td><td>".$value."</td></tr>";
+    							
+		$instructions .= "</table></td></tr></table></div>";
+		$this->instruction->setText($instructions);
+	}
 }
