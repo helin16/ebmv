2e0ed99b1871fa535a57c68e770bb40f7bad077a
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-14 04:13:31 +0000

    added function for product attributes

--- core/main/service/ProductAttributeTypeService.php
+++ core/main/service/ProductAttributeTypeService.php
@@ -28,5 +28,24 @@ class ProductAttributeTypeService extends BaseService
         $types = $this->findByCriteria('code = ?', array($code), true, 1, 1);
         return count($types) > 0 ? $types[0] : null;
     }
+    /**
+    * Getting the product type by code
+    *
+    * @param array $code The code we are searching on
+    *
+    * @return array
+    */
+    public function getTypesByCodes(array $codes)
+    {
+        if(count($codes) === 0)
+            return array();
+        $results = array();
+        $types = $this->findByCriteria('code in (' . implode(', ', array_fill(0, count($codes), '?')) . ')', $codes);
+        foreach($types as $type)
+        {
+            $results[$type->getCode()] = $type;
+        }
+        return $results;
+    }
 }
 ?>
