b99f3ad18a921537315b999c4d74d10ae8c447bd
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-14 23:37:31 +0000

    added table Asset for images

--- core/promotion/initial.sql
+++ core/promotion/initial.sql
@@ -1,9 +1,11 @@
 -- Setting Up Database
-DROP TABLE IF EXISTS `session`;
-CREATE TABLE `session` (
+DROP TABLE IF EXISTS `asset`;
+CREATE TABLE `asset` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `key` varchar(32) NOT NULL DEFAULT '',
-    `data` longtext NOT NULL ,
+    `assetId` varchar(32) NOT NULL DEFAULT '',
+    `filename` varchar(100) NOT NULL DEFAULT '',
+    `mimeType` varchar(50) NOT NULL DEFAULT '',
+    `data` MEDIUMTEXT NOT NULL ,
     `active` bool NOT NULL DEFAULT 1,
     `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
     `createdById` int(10) unsigned NOT NULL DEFAULT 0,
@@ -12,65 +14,7 @@ CREATE TABLE `session` (
     PRIMARY KEY (`id`)
     ,INDEX (`createdById`)
     ,INDEX (`updatedById`)
-    ,UNIQUE INDEX (`key`)
-) ENGINE=innodb DEFAULT CHARSET=utf8;
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(50) NOT NULL DEFAULT '',
-    `active` bool NOT NULL DEFAULT 1,
-    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
-    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
-    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-    ,INDEX (`createdById`)
-    ,INDEX (`updatedById`)
-    ,UNIQUE INDEX (`name`)
-) ENGINE=innodb DEFAULT CHARSET=utf8;
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE `person` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `firstName` varchar(50) NOT NULL DEFAULT '',
-    `lastName` varchar(50) NOT NULL DEFAULT '',
-    `active` bool NOT NULL DEFAULT 1,
-    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
-    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
-    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-    ,INDEX (`createdById`)
-    ,INDEX (`updatedById`)
-    ,INDEX (`firstName`)
-    ,INDEX (`lastName`)
-) ENGINE=innodb DEFAULT CHARSET=utf8;
-DROP TABLE IF EXISTS `useraccount`;
-CREATE TABLE `useraccount` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `username` varchar(100) NOT NULL DEFAULT '',
-    `password` varchar(40) NOT NULL DEFAULT '',
-    `personId` int(10) unsigned NOT NULL DEFAULT 0,
-    `active` bool NOT NULL DEFAULT 1,
-    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
-    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
-    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-    ,INDEX (`personId`)
-    ,INDEX (`createdById`)
-    ,INDEX (`updatedById`)
-    ,INDEX (`password`)
-    ,UNIQUE INDEX (`username`)
-) ENGINE=innodb DEFAULT CHARSET=utf8;
-DROP TABLE IF EXISTS `role_useraccount`;
-CREATE TABLE `role_useraccount` (
-    `roleId` int(10) unsigned NOT NULL,
-    `useraccountId` int(10) unsigned NOT NULL,
-    `created` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    `createdById` int(10) unsigned NOT NULL,
-    UNIQUE KEY `uniq_role_useraccount` (`roleId`,`useraccountId`),
-    KEY `idx_role_useraccount_roleId` (`roleId`),
-    KEY `idx_role_useraccount_useraccountId` (`useraccountId`)
+    ,UNIQUE INDEX (`assetId`)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
@@ -152,6 +96,79 @@ CREATE TABLE `productattributetype` (
     ,INDEX (`name`)
     ,INDEX (`searchable`)
     ,UNIQUE INDEX (`code`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `firstName` varchar(50) NOT NULL DEFAULT '',
+    `lastName` varchar(50) NOT NULL DEFAULT '',
+    `active` bool NOT NULL DEFAULT 1,
+    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+    ,INDEX (`createdById`)
+    ,INDEX (`updatedById`)
+    ,INDEX (`firstName`)
+    ,INDEX (`lastName`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL DEFAULT '',
+    `active` bool NOT NULL DEFAULT 1,
+    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+    ,INDEX (`createdById`)
+    ,INDEX (`updatedById`)
+    ,UNIQUE INDEX (`name`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `key` varchar(32) NOT NULL DEFAULT '',
+    `data` longtext NOT NULL ,
+    `active` bool NOT NULL DEFAULT 1,
+    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+    ,INDEX (`createdById`)
+    ,INDEX (`updatedById`)
+    ,UNIQUE INDEX (`key`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `useraccount`;
+CREATE TABLE `useraccount` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(100) NOT NULL DEFAULT '',
+    `password` varchar(40) NOT NULL DEFAULT '',
+    `personId` int(10) unsigned NOT NULL DEFAULT 0,
+    `active` bool NOT NULL DEFAULT 1,
+    `created` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+    `createdById` int(10) unsigned NOT NULL DEFAULT 0,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updatedById` int(10) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+    ,INDEX (`personId`)
+    ,INDEX (`createdById`)
+    ,INDEX (`updatedById`)
+    ,INDEX (`password`)
+    ,UNIQUE INDEX (`username`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `role_useraccount`;
+CREATE TABLE `role_useraccount` (
+    `roleId` int(10) unsigned NOT NULL,
+    `useraccountId` int(10) unsigned NOT NULL,
+    `created` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    `createdById` int(10) unsigned NOT NULL,
+    UNIQUE KEY `uniq_role_useraccount` (`roleId`,`useraccountId`),
+    KEY `idx_role_useraccount_roleId` (`roleId`),
+    KEY `idx_role_useraccount_useraccountId` (`useraccountId`)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 
 -- Completed CRUD Setup.
