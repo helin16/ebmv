711a84f46a54679a4c28fe7a6487af09671111c4
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-15 01:25:29 +0000

    added updateCategory() to category service

--- core/main/service/CategoryService.php
+++ core/main/service/CategoryService.php
@@ -16,5 +16,24 @@ class CategoryService extends BaseServiceAbastract
     {
         parent::__construct("Category");
     }
+    /**
+     * Find or create a category with the same name
+     * 
+     * @param string   $categoryName The name of the category
+     * @param Category $parent       The parent category
+     * 
+     * @return Category
+     */
+    public function updateCategory($categoryName, Category $parent = null)
+    {
+        $category = $this->findByCriteria('name = ?', trim($categoryName), true, 1, 1);
+        if(count($category) > 0)
+            return $category;
+        
+        $category = new Category();
+        $category->setName($categoryName);
+        $category->setParent($parent);
+        return $this->save($category);
+    }
 }
 ?>
