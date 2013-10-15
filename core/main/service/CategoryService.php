ed9c69a027d5b737fad10c6b7d98a376e9e9e315
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-15 01:52:54 +0000

    added function moveCategory() to categoryservice

--- core/main/service/CategoryService.php
+++ core/main/service/CategoryService.php
@@ -35,5 +35,49 @@ class CategoryService extends BaseServiceAbastract
         $category->setParent($parent);
         return $this->save($category);
     }
+    /**
+     * move the category to another
+     * 
+     * @param Category $category The moving category
+     * @param Caregory $parent   The target category
+     * 
+     * @return CategoryService
+     */
+    public function moveCategory(Category &$category, Category $parent)
+    {
+        $transStarted = false;
+        try { Dao::beginTransaction();} catch (Exception $ex) {$transStarted = true; }
+        try
+        {
+            
+            if(($pos = trim($category->getPosition())) === '' || $pos === '1')
+                $category = $this->save($category);
+            
+            $newPos = $parent->getNextPosition();
+            $this->updateByCriteria('set position = CONCAT(:newPos, substring(position, :posLen)), rootId = :newRootId', 'rootId = :rootId and position like :oldPos', 
+                array(
+            		'newPos' => $newPos, 
+            		'oldPos' => $pos, 
+            		'posLen' => strlen($pos) + 1,
+            		'newRootId' => $parent->getRoot()->getId(),
+            		'rootId' => $category->getRoot()->getId()
+            	)
+            );
+            
+            $category = $this->get($category->getId());
+            $category->setPosition($parent->getNextPosition());
+            $category->setParent($parent);
+            $category->setRoot($parent->getRoot());
+            $category = $this->save($category);
+            return $category;
+        }
+        catch(Exception $ex)
+        {
+        if($transStarted === false)
+            Dao::rollbackTransaction();
+            throw $ex;
+        }
+        
+    }
 }
 ?>
