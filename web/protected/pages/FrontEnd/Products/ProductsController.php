319e2e3b6205fe59cf33e31f6a3649471f08fd4b
murahman2008@gmail.com <murahman2008@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-21 05:59:18 +0000

    adding Advance Search Option on php

--- web/protected/pages/FrontEnd/Products/ProductsController.php
+++ web/protected/pages/FrontEnd/Products/ProductsController.php
@@ -22,7 +22,16 @@ class ProductsController extends FrontEndPageAbstract
 		$js .= 'var pageJs = new PageJs("productlist", "' . $this->getProductsBtn->getUniqueID() . '"); ';
 		$js .= 'pageJs.pagination.pageSize = ' . $this->pageSize . ';';
 		$js .= (($catId = trim($this->Request['cateid'])) === '' ? '' : 'pageJs.searchCriteria.categoryIds.push(' . $catId . ');');
-		$js .= (($searchtext = trim($this->Request['searchtext'])) === '' ? '' : 'pageJs.searchCriteria.searchString = "' . $searchtext . '";');
+		if(($searchtext = trim($this->Request['searchtext'])) !== '')
+		{
+			if (($searchInfo = json_decode($searchtext, true)) !== null)
+			{
+				$searchtext = trim($searchInfo['searchText']);
+				$js .= 'pageJs.searchCriteria.searchCat = "'.trim($searchInfo['searchCat']).'";';
+				$js .= 'pageJs.searchCriteria.searchOpt = "' . trim($searchInfo['searchOpt']) . '";';
+			}
+			$js .= 'pageJs.searchCriteria.searchString = "' . $searchtext . '";';
+		}
 		$js .= 'pageJs.showProducts();';
 	   return $js;
 	}
@@ -92,15 +101,19 @@ class ProductsController extends FrontEndPageAbstract
 	        }
 	        
 	        $categoryIds = array();
-	        $searchText = '';
+	        $searchText = $searchOption = $searchCategory = '';
 	        if(isset($params->CallbackParameter->searchCriteria))
 	        {
 	            $searchCriteria = json_decode(json_encode($params->CallbackParameter->searchCriteria), true);
 	            $searchText = $searchCriteria['searchString'];
 	            $categoryIds = is_array($searchCriteria['categoryIds']) ? $searchCriteria['categoryIds'] : array();
+	            $searchOption = (isset($searchCriteria['searchOpt']) && trim($searchCriteria['searchOpt']) !== '' && trim($searchCriteria['searchOpt']) !== '0') ? $searchCriteria['searchOpt'] : '';
+	            $searchCategory = (isset($searchCriteria['searchCat']) && trim($searchCriteria['searchCat']) !== '' && trim($searchCriteria['searchCat']) !== '0') ? $searchCriteria['searchCat'] : '';
+	            if($searchCategory !== '')
+	            	$categoryIds[] = $searchCategory;
 	        }
 	        
-	        $products = BaseServiceAbastract::getInstance('Product')->findProductsInCategory($searchText, $categoryIds, false, $pageNo, $pageSize, array());
+	        $products = BaseServiceAbastract::getInstance('Product')->findProductsInCategory($searchText, $categoryIds, $searchOption,  false, $pageNo, $pageSize, array());
 	        $result['pagination'] = BaseServiceAbastract::getInstance('Product')->getPageStats();
 	        $result['products'] = array();
 	        foreach($products as $product)
