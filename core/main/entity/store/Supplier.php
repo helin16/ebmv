0bfe9428c9ae16c15456759bdf4a5a928002695a
murahman2008@gmail.com <murahman2008@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-23 03:37:38 +0000

    added new function validateScheduledTime() to validate the scheduledTime

--- core/main/entity/store/Supplier.php
+++ core/main/entity/store/Supplier.php
@@ -8,7 +8,13 @@
  */
 class Supplier extends BaseEntityAbstract
 {
-    /**
+    const MINUTE_REGEX = '/^(\*|[0-9]{1}|[1-5][0-9])$/';
+    const HOUR_REGEX = '/^(\*|[0-9]{1}|[1][0-9]|[2][0-3])$/';
+    const DOM_REGEX = '/^(\*|[1-9]{1}|[1][0-9]|[2][0-9]|[3][0-1])$/';
+    const MONTH_REGEX = '/^(\*|[1-9]{1}|[1][0-2])$/';
+    const DOW_REGEX = '/^(\*|[0-7]{1})$/';
+
+	/**
      * The name of the supplier
      * 
      * @var string
@@ -126,8 +132,38 @@ class Supplier extends BaseEntityAbstract
 	 */
 	public function setScheduledTime($scheduledTime)
 	{
-	    $this->scheduledTime = $scheduledTime;
+	    if(self::validateScheduledTime($scheduledTime) === true)
+			$this->scheduledTime = $scheduledTime;
+			
 	    return $this;
+	}
+	
+	/**
+	 * Validate the Scheduled time based on the time format of the UNIX crontab
+	 *
+	 * @param String $scheduledTime
+	 * 
+	 * @return boolean
+	 */
+	public static function validateScheduledTime($scheduledTime)
+	{
+		$errorMessage = 'Valid format should be 1 2 3 4 5 WHERE 1 = Minute (0-59)/*, 2 = Hour (0-23)/*, 3= Day of the Month (1-31)/*, 4 = Month(1-12)/*, 5 = Day of week (0-7)/*';
+		$stArray = explode(' ', $scheduledTime);
+		if(count($stArray) !== 5)
+			throw new Exception($scheduledTime.' is not valid.'.$errorMessage);
+
+		if(preg_match(self::MINUTE_REGEX, trim($stArray[0])) === 0)
+			throw new Exception('Minute on scheduled time ['.$scheduledTime.'] is NOT valid.'.$errorMessage);
+		if(preg_match(self::HOUR_REGEX, trim($stArray[1])) === 0)
+			throw new Exception('Hour on scheduled time ['.$scheduledTime.'] is NOT valid.'.$errorMessage);
+		if(preg_match(self::DOM_REGEX, trim($stArray[2])) === 0)
+			throw new Exception('Day of Month on scheduled time ['.$scheduledTime.'] is NOT valid.'.$errorMessage);
+		if(preg_match(self::MONTH_REGEX, trim($stArray[3])) === 0)
+			throw new Exception('Month on scheduled time ['.$scheduledTime.'] is NOT valid.'.$errorMessage);
+		if(preg_match(self::DOW_REGEX, trim($stArray[4])) === 0)
+			throw new Exception('Day of Week on scheduled time ['.$scheduledTime.'] is NOT valid.'.$errorMessage);
+			
+		return true;	
 	}
 	
 	/**
