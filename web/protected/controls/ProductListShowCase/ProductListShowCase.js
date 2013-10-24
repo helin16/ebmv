5f72e584bd19a6a1980cd19b7571a7fa110fe180
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-24 05:13:21 +0000

    added JCarousel onto homepage

--- web/protected/controls/ProductListShowCase/ProductListShowCase.js
+++ web/protected/controls/ProductListShowCase/ProductListShowCase.js
@@ -18,7 +18,7 @@ ProductListShowCaseJs.prototype = Object.extend(new FrontPageJs(), {
 				tmp.listDiv = new Element('ul',{'class': 'jcarousel jcarousel-skin-tango'});
 				try {
 					tmp.result = tmp.me.getResp(param, false, true);
-					if(tmp.result.products.size() > 0) 
+					if(tmp.result.products.size() === 0) 
 						throw 'No product found!';
 					tmp.result.products.each(function(item){
 						tmp.listDiv.insert({'bottom': tmp.me._getProductThumbnail(item).wrap(new Element('li')) });
