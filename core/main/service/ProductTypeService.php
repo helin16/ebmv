63673364f877823991c83c8201dbcd511cbcb5b3
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-10-24 06:23:52 +0000

    added producttype service

---
+++ core/main/service/ProductTypeService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ProductTypeService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class ProductTypeService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("ProductType");
+    }
+}
+?>
