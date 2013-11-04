21a43e02d50502d43843d4d380c27c5eeee9dc4f
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-04 06:29:47 +0000

    aded for download book

--- core/main/service/SupplierService.php
+++ core/main/service/SupplierService.php
@@ -16,5 +16,20 @@ class SupplierService extends BaseServiceAbastract
     {
         parent::__construct("Supplier");
     }
+    /**
+     * Getting the cheapest supplier for a product
+     * 
+     * @param Product $product The product
+     * 
+     * @return Supplier
+     */
+    public function getCheapestSupplier(Product $product)
+    {
+    	$sql = 'select supplierId from supplierprice where active = 1 and productId = ? order by price asc limit 1';
+    	$result = Dao::getResultsNative($sql, array($product->getId()));
+    	if(count($result) === 0)
+    		return null;
+    	return $this->get($result[0]['supplierId']);
+    }
 }
 ?>
