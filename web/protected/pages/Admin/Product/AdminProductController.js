a480b7077bf2ad79678fb86111a194ce74dc82d0
murahman2008@gmail.com <murahman2008@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-06 05:16:10 +0000

    admin product page edit option

--- web/protected/pages/Admin/Product/AdminProductController.js
+++ web/protected/pages/Admin/Product/AdminProductController.js
@@ -14,9 +14,13 @@ PageJs.prototype = Object.extend(new CrudPageJs(), {
 			tmp.resultDiv.insert({'bottom':  tmp.me._getItemRow('id', 'suk', 'title', 'active', 'option').addClassName('titleRow') });
 		tmp.i = (itemrowindex || 0);
 		items.each(function(item) {
-			tmp.resultDiv.insert({'bottom':  tmp.me._getItemRow(item.id, item.suk, item.title, item.active,  new Element('img', {'editId': item.id, 'class': 'btn', 'src': '/themes/default/images/edit.png', 'alt': 'EDIT'})
-				.observe('click', function() {tmp.me.editItem(this); }) ).addClassName(tmp.i % 2 === 1 ? 'even' : 'odd')
+			tmp.oneRowDiv = new Element('div', {'class' : 'singleRowDiv'});
+			tmp.viewDiv = new Element('div', {'class' : 'viewProductDiv'});
+			tmp.viewDiv.insert({'bottom':  tmp.me._getItemRow(item.id, item.suk, item.title, item.active,  new Element('img', {'editId': item.id, 'class': 'btn', 'src': '/themes/default/images/edit.png', 'alt': 'EDIT'})
+				.observe('click', function() {tmp.me.editItem(this); }) )
 			});
+			tmp.oneRowDiv.insert({'bottom' : tmp.viewDiv}).addClassName(tmp.i % 2 === 1 ? 'even' : 'odd');
+			tmp.resultDiv.insert({'bottom': tmp.oneRowDiv});
 			tmp.i++;
 		});
 		return tmp.resultDiv;
@@ -44,22 +48,63 @@ PageJs.prototype = Object.extend(new CrudPageJs(), {
 		tmp.active = tmp.btnSpan.previous('span.col.active').innerHTML;
 		tmp.id = tmp.btnSpan.previous('span.col.id').innerHTML;
 		
+		tmp.activeOptions1 = new Element('option', {'value': 'true'}).update('true');
+		tmp.activeOptions2 = new Element('option', {'value': 'false'}).update('false');
+		
 		tmp.editPanel = new Element('div', {'class' : 'editProductDiv'})
 							.insert({'bottom' : new Element('span', {'class' : 'col id'}).update(tmp.id) })
 							.insert({'bottom' : new Element('span', {'class' : 'eSukLabel'}).update('Suk') })
 							.insert({'bottom' : new Element('input', {'type' : 'text', 'class' : 'eSukBox rdcrnr lightBrdr', 'value' : tmp.suk}) })
 							.insert({'bottom' : new Element('span', {'class' : 'eTitleLabel'}).update('Title') })
 							.insert({'bottom' : new Element('input', {'type' : 'text', 'class' : 'eTitleBox rdcrnr lightBrdr', 'value' : tmp.title}) })
-							.insert({'bottom' : new Element('select', {'class' : 'eActiveBox rdcrnr lightBrdr'}) })
+							.insert({'bottom' : new Element('select', {'class' : 'eActiveBox rdcrnr lightBrdr'}) 
+									.insert(tmp.activeOptions1)
+									.insert(tmp.activeOptions2)
+							})
 							.insert({'bottom' : new Element('span', {'class' : 'col btns'}) 
-								.insert({'bottom' : new Element('img', {'class' : 'editBtn', 'src': '/themes/default/images/save.png', 'alt': 'Save', 'title' : 'Save'}) })
-								.insert({'bottom' : new Element('img', {'class' : 'cancelBtn', 'src': '/themes/default/images/cancel.gif', 'alt': 'Cancel', 'title' : 'Cancel'}) })
-								
+								.insert({'bottom' : new Element('img', {'class' : 'editBtn', 'src': '/themes/default/images/save.png', 'alt': 'Save', 'title' : 'Save'})
+									.observe('click', function() {tmp.me.saveEditedItem(this); }) })
+								.insert({'bottom' : new Element('img', {'class' : 'cancelBtn', 'src': '/themes/default/images/cancel.gif', 'alt': 'Cancel', 'title' : 'Cancel'})
+									.observe('click', function() {tmp.me.cancelEdit(this); })
+								})
 						});
 		
-		tmp.btnSpan.up('div.row').update(tmp.editPanel);
+		tmp.btnSpan.up('div.singleRowDiv').insert({'bottom' : tmp.editPanel}).down('div.viewProductDiv').hide();
+	}
+	
+	,cancelEdit: function(btn) {
+		var tmp = {};
+		tmp.me = this;
 		
-		
+		tmp.me._hideShowAllEditPens(btn, true);
+		tmp.rowDiv = $(btn).up('div.singleRowDiv');
+		tmp.rowDiv.down('div.viewProductDiv').show();
+		tmp.rowDiv.down('div.editProductDiv').remove();
+	}
+	
+	,saveEditedItem: function(btn) {
+		var tmp = {};
+		tmp.me = this;
+		try
+		{
+			tmp.editDiv = $(btn).up('div.editProductDiv');
+			tmp.editProductId = tmp.editDiv.down('span.col.id').innerHTML;
+			tmp.eSukValue = tmp.editDiv.down('span.eSukBox').getValue();
+			//tmp.eTitleValue = tmp.editDiv.down('span.eTitleBox').value.trim();
+			//tmp.eActiveValue = tmp.editDiv.down('span.eActiveBox').value;
+			
+			//console.debug(tmp.eSukValue);
+			
+			if(eSukValue === '' || eSukValue === undefined)
+				throw 'SUK must be provided';
+			if(eTitleValue === '' || eTitleValue === undefined)
+				throw 'Title must be provided';
+		}
+		catch(e)
+		{
+			alert(e);
+		}
+		tmp.editDiv.down('span.col.id');
 		
 	}
 
