4519e5079feac033b0884bdf5e7b3d6d619627ae
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-14 03:31:06 +0000

    changed admin product js

--- web/protected/pages/Admin/Product/AdminProductController.js
+++ web/protected/pages/Admin/Product/AdminProductController.js
@@ -4,6 +4,8 @@
 var PageJs = new Class.create();
 PageJs.prototype = Object.extend(new CrudPageJs(), {
 	
+	types: null, //the attribute types
+	
 	_getResultDiv: function(items, includetitlerow, itemrowindex) {
 		var tmp = {};
 		tmp.me = this;
@@ -11,60 +13,57 @@ PageJs.prototype = Object.extend(new CrudPageJs(), {
 		
 		tmp.resultDiv = new Element('div');
 		if(tmp.includetitlerow === true)
-			tmp.resultDiv.insert({'bottom':  tmp.me._getItemRow('id', 'suk', 'title', 'active', 'option').addClassName('titleRow') });
+			tmp.resultDiv.insert({'bottom':  tmp.me._getItemRow({'id': 'id', 'suk': 'suk', 'title': 'title', 'active': 'active', 'istitle': true}, 'option').addClassName('titleRow') });
 		tmp.i = (itemrowindex || 0);
 		items.each(function(item) {
-			tmp.oneRowDiv = new Element('div', {'class' : 'singleRowDiv'});
-			tmp.viewDiv = new Element('div', {'class' : 'viewProductDiv'}).store('main_item', item);
-			tmp.viewDiv.insert({'bottom':  tmp.me._getItemRow(item.id, item.suk, item.title, item.active,  new Element('img', {'editId': item.id, 'class': 'btn', 'src': '/themes/default/images/edit.png', 'alt': 'EDIT'})
-				.observe('click', function() {tmp.me.editItem(this); }) )
-			}).insert({'bottom' : tmp.me._getAdditionalProductInfo(item) });
-			tmp.oneRowDiv.insert({'bottom' : tmp.viewDiv}).addClassName(tmp.i % 2 === 1 ? 'even' : 'odd');
-			tmp.resultDiv.insert({'bottom': tmp.oneRowDiv});
+			tmp.resultDiv.insert({'bottom':  tmp.me._getItemRow(item, new Element('img', {'editId': item.id, 'class': 'btn', 'src': '/themes/default/images/edit.png', 'alt': 'EDIT'})
+				.observe('click', function() {tmp.me.editItem(this); }) ).addClassName(tmp.i % 2 === 1 ? 'even' : 'odd')
+			});
 			tmp.i++;
 		});
 		return tmp.resultDiv;
 	}
 
-	,_getItemRow: function (id, suk, title, active, option) {
+	,_getItemRow: function (item, option) {
 		var tmp = {};
 		tmp.me = this;
-		tmp.div = new Element('div', {'class' : 'row'})
-			.insert({'bottom' : new Element('span', {'class' : 'col id'}).update(id) })
-			.insert({'bottom' : new Element('span', {'class' : 'col suk'}).update(suk) })	
-			.insert({'bottom' : new Element('span', {'class' : 'col title'}).update(title) })	
-			.insert({'bottom' : new Element('span', {'class' : 'col active'}).update(active) })	
-			.insert({'bottom' : new Element('span', {'class' : 'col btns'}).update(option) });
+		tmp.div = new Element('div', {'class' : 'row singleRowDiv'}).store('main_item', item)
+			.insert({'bottom': new Element('div', {'class': 'viewProductDiv'})
+				.insert({'bottom' : new Element('span', {'class' : 'col id'}).update(item.id) })
+				.insert({'bottom' : new Element('span', {'class' : 'col suk'}).update(item.sku) })	
+				.insert({'bottom' : new Element('span', {'class' : 'col title', 'item': 'title'}).update(item.title) })	
+				.insert({'bottom' : new Element('span', {'class' : 'col active', 'item': 'active',  'itemedittype': 'checkbox'}).update(item.active) })	
+				.insert({'bottom' : new Element('span', {'class' : 'col btns'}).update(option) })
+			})
+			.insert({'bottom' : tmp.me._getAdditionalProductInfo(item) });
 		return tmp.div;
 	}
 	
-	,_getAdditionalProductInfo: function (item) {
+	,_getAdditionalProductInfo: function (item, isedit) {
 		var tmp = {};
 		tmp.me = this;
-		
-		tmp.outputArray = new Hash();
-		
-		console.debug(item);
-		tmp.attributeArray = $H(item.attributes);
-		tmp.keyArray = tmp.attributeArray.keys();
-		tmp.keyArray.each(function(key) {
-			tmp.attributeArray.get(key).each(function(item) {
-				if(!tmp.outputArray[item.type.code + '~' + item.type.name])
-					tmp.outputArray[item.type.code + '~' + item.type.name] = new Array();
-				tmp.outputArray[item.type.code + '~' + item.type.name].push(item.attribute);
-			})
-		});
-		
-		tmp.div = new Element('div');
-		console.debug(tmp.outputArray);
-		
-		tmp.outputArray.keys().each(function(key) {
-			console.debug(key);
-			tmp.div.insert({'bottom' : new Element('span').update(key)});
-			tmp.outputArray.get(key).each(function(value) {
-				tmp.div.insert({'bottom' : new Element('span').update(value)});
-			});
-			tmp.div.insert({'bottom' : new Element('span').update('--------------------------')});
+		tmp.div = new Element('div', {'class': 'attrs_wrapper'});
+		tmp.code = '';
+		$H(item.attributes).each(function(itemArr) {
+			tmp.attrCode = itemArr.key;
+			if(typeof(itemArr.value) === 'object') {
+				tmp.attrDiv = new Element('div', {'class': 'attr_wrapper'}); 
+				
+				//getting the title div
+				tmp.attrDiv.insert({'bottom': new Element('span', {'class': 'attr_name inlineblock'}).update(itemArr.value[0].type.name) });
+				if(tmp.attrCode !== tmp.code) {
+					tmp.code = tmp.attrCode;
+				} 
+				
+				//getting the value div
+				tmp.attrValeusDiv = new Element('span', {'class': 'attr_values_wrapper inlineblock'}); 
+				itemArr.value.each(function(attr) {
+					tmp.attrValeusDiv.insert({'bottom': new Element('div', {'class': 'attr_value' }).update(attr.attribute) });
+				});
+				tmp.attrDiv.insert({'bottom': tmp.attrValeusDiv });
+				
+				tmp.div.insert({'bottom': tmp.attrDiv });
+			};
 		});
 		return tmp.div;
 	}
