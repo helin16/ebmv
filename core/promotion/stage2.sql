c25f12a73848d9a4a6d556e0f0e353f797010e92
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-15 04:48:13 +0000

    changed ImportProduct Cronjob to import for all suppliers

--- core/promotion/stage2.sql
+++ core/promotion/stage2.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `language` ADD UNIQUE INDEX `code`(`code`);
 ALTER TABLE `producttype` DROP INDEX `name`, ADD UNIQUE INDEX `name` USING BTREE(`name`);
 
 update `language` set `code` = 'zh-CN' where id = 1;
