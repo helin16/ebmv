49a5ae7719bbfe0f7671aecfd710a0b0b7068f9e
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-18 04:57:02 +0000

    added supplier connector to supplier entity

--- core/promotion/stage2-supplierconnector.sql
+++ core/promotion/stage2-supplierconnector.sql
@@ -1,5 +1,5 @@
-update supplier set connector='SC_XinHua' where id = 1;
-
 ALTER TABLE `bmv`.`supplier` CHANGE COLUMN `supplierLocation` `connector` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
  DROP INDEX `supplierLocation`,
  ADD INDEX `supplierLocation` USING BTREE(`connector`);
+ 
+update supplier set connector='SC_XinHua' where id = 1;
