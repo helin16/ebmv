2380a351ee9e7ee6d07892d2144536321799621e
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-22 04:17:04 +0000

    created SupplierInfoService

---
+++ core/main/service/SupplierInfoService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * SupplierInfoService
+ *
+ * @package    Core
+ * @subpackage Service
+ * @author     lhe<helin16@gmail.com>
+ *
+ */
+class SupplierInfoService extends BaseServiceAbastract
+{
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct("SupplierInfo");
+    }
+}
+?>
