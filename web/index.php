2009006064b834bb37769d50586a2028947546a5
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-11-27 05:03:40 +0000

    enforce https on index.php

--- web/index.php
+++ web/index.php
@@ -18,9 +18,8 @@ try
 }
 catch(Exception $e)
 {
-	header("HTTP/1.0 404 Not Found");
-	echo "<h1>404 Not Found</h1>";
-    echo "The page that you have requested could not be found.";
+	header("HTTP/1.0 403 Unauthorized");
+	echo "Unauthorized request!";
     exit();
 }
 
