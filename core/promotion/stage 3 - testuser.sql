497e5726e04515dc761c722d61839dfe94953992
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-12-03 03:01:01 +0000

    creating testuser for YL

---
+++ core/promotion/stage 3 - testuser.sql
@@ -0,0 +1,3 @@
+insert into `person`(`id`, `firstName`, `lastName`, `active`, `created`, `createdById`, `updated`, `updatedById`) values (2, 'test user', 'YL', 1, NOW(), 100, NOW(), 100);
+insert into `useraccount`(`id`, `username`, `password`, `personId`, `libraryId`, `active`, `created`, `createdById`, `updated`, `updatedById`) values (2, 'testuser_yl', sha1('testpass_yl'), '2', '37', 1, NOW(), 100, NOW(), 100);
+insert into `role_useraccount` (`roleId`, `userAccountId`, `created`, `createdById`) values (2, 2, NOW(), 100);
