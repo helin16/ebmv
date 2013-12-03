cfea55e7b42cf0b9d67d4a2afcc3b8144cf6a48a
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-12-03 04:03:56 +0000

    added function getLibsFromCodes() onto LibraryService

--- core/main/service/LibraryService.php
+++ core/main/service/LibraryService.php
@@ -29,7 +29,22 @@ class LibraryService extends BaseServiceAbastract
      */
     public function getLibsFromCode($code, $searchActiveOnly = true, $pageNo = null, $pageSize = DaoQuery::DEFAUTL_PAGE_SIZE, $orderBy = array())
     {
-    	return $this->_getLibsFromInfo($code, 'aus_code', $searchActiveOnly, $pageNo, $pageSize, $orderBy);
+    	return $this->_getLibsFromInfo(array($code), 'aus_code', $searchActiveOnly, $pageNo, $pageSize, $orderBy);
+    }
+    /**
+     * Getting the librarys from the codes
+     * 
+     * @param array  $codes
+     * @param bool   $searchActiveOnly
+     * @param int    $pageNo
+     * @param int    $pageSize
+     * @param array  $orderBy
+     * 
+     * @return Ambigous <multiple:Library, Ambigous, multitype:, multitype:BaseEntityAbstract >
+     */
+    public function getLibsFromCodes(array $codes, $searchActiveOnly = true, $pageNo = null, $pageSize = DaoQuery::DEFAUTL_PAGE_SIZE, $orderBy = array())
+    {
+    	return $this->_getLibsFromInfo($codes, 'aus_code', $searchActiveOnly, $pageNo, $pageSize, $orderBy);
     }
    /**
     * Getting the libraray from the code
@@ -45,7 +60,7 @@ class LibraryService extends BaseServiceAbastract
     }
     /**
      * Getting the Libraries for the Li
-     * @param string $infoValue        The value we are searching for
+     * @param array  $infoValues       The value we are searching for
      * @param int    $typeCode         The code of the type for the information
      * @param bool   $searchActiveOnly Whether active only
      * @param int    $pageNo           page number
@@ -54,11 +69,12 @@ class LibraryService extends BaseServiceAbastract
      * 
      * @return multiple:Library
      */
-    private function _getLibsFromInfo($infoValue, $typeCode, $searchActiveOnly = true, $pageNo = null, $pageSize = DaoQuery::DEFAUTL_PAGE_SIZE, $orderBy = array())
+    private function _getLibsFromInfo($infoValues, $typeCode, $searchActiveOnly = true, $pageNo = null, $pageSize = DaoQuery::DEFAUTL_PAGE_SIZE, $orderBy = array())
     {
     	$query = EntityDao::getInstance('Library')->getQuery();
     	$query->eagerLoad('Library.infos', DaoQuery::DEFAULT_JOIN_TYPE, 'lib_info')->eagerLoad('LibraryInfo.type', DaoQuery::DEFAULT_JOIN_TYPE, 'lib_info_type');
-    	return $this->findByCriteria('lib_info_type.code = ? and lib_info.value = ?', array($typeCode, $infoValue), $searchActiveOnly, $pageNo, $pageSize, $orderBy);
+    	$params = array_merge(array($typeCode), $infoValues);
+    	return $this->findByCriteria('lib_info_type.code = ? and lib_info.value in (' . implode(', ', array_fill(0, count($infoValues), '?')) . ')', $params, $searchActiveOnly, $pageNo, $pageSize, $orderBy);
     }
     /**
      * Getting the library by the url
@@ -69,7 +85,7 @@ class LibraryService extends BaseServiceAbastract
      */
     public function getLibByURL($url)
     {
-    	$result = $this->_getLibsFromInfo($url, 'lib_url', true, 1, 1);
+    	$result = $this->_getLibsFromInfo(array($url), 'lib_url', true, 1, 1);
     	return (count($result) === 0 ? null : $result[0]);
     }
 }
