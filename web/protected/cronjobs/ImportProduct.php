153f29111d60daa646ed3bcaed1c234a9c1fed12
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-12-05 06:57:26 +0000

    removed SupplierPrice and added Supplier Column onto Product

--- web/protected/cronjobs/ImportProduct.php
+++ web/protected/cronjobs/ImportProduct.php
@@ -108,17 +108,17 @@ class ImportProduct
 }
 
 //checking usage
-// if ($argc != 4)
-// 	die("Usage: ImportProduct siteCode(37,werew,121fd|all) supplierids(1,2,3|all) totalrecords(30|all)\r\n");
+if ($argc != 4)
+	die("Usage: ImportProduct siteCode(37,werew,121fd|all) supplierids(1,2,3|all) totalrecords(30|all)\r\n");
 
-// $libCodes = (($libCodes = trim($argv[1])) === 'all' ? array() : explode(',', str_replace(' ', '', $libCodes)));
-// $supplierIds = (($supplierIds = trim($argv[2])) === 'all' ? array() : explode(',', str_replace(' ', '', $supplierIds)));
-// $totalrecords = (($totalrecords = trim($argv[3])) === 'all' ? null : $totalrecords);
+$libCodes = (($libCodes = trim($argv[1])) === 'all' ? array() : explode(',', str_replace(' ', '', $libCodes)));
+$supplierIds = (($supplierIds = trim($argv[2])) === 'all' ? array() : explode(',', str_replace(' ', '', $supplierIds)));
+$totalrecords = (($totalrecords = trim($argv[3])) === 'all' ? null : $totalrecords);
 
-// ImportProduct::log("== Params ===================================================\r\n");
-// ImportProduct::log("== Site Codes: '" . implode("', '", $libCodes). "'\r\n");
-// ImportProduct::log("== Supplier IDS: " . implode(', ', $supplierIds). "\r\n");
-// ImportProduct::log("== Total Records: '" . $totalrecords. "'\r\n");
-// ImportProduct::log("=============================================================\r\n");
+ImportProduct::log("== Params ===================================================\r\n");
+ImportProduct::log("== Site Codes: '" . implode("', '", $libCodes). "'\r\n");
+ImportProduct::log("== Supplier IDS: " . implode(', ', $supplierIds). "\r\n");
+ImportProduct::log("== Total Records: '" . $totalrecords. "'\r\n");
+ImportProduct::log("=============================================================\r\n");
 
-ImportProduct::run(array('37'), array(2), 10);
+ImportProduct::run($libCodes, $supplierIds, $totalrecords);
