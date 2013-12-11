0cffb392664e51a4c285cf2cede0511f78d75f11
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2013-12-11 07:38:54 +0000

    trimming the response from WebAuth

--- web/test.php
+++ web/test.php
@@ -2,4 +2,4 @@
 $wsdl ='https://ebmv.com.au/?soap=webauth.wsdl';
 $client = new SoapClient($wsdl, array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
 $result = $client->authenticate('A3ADC78482897208E84B759E41DD73E9', '37', 'testuser_yl', '2A2877E4DF17AEED392AA42AD36EE5190E1E1DCC');
-var_dump($result);
+echo $result;
