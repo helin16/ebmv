2d97b00c703e5447d2f0493414380ccaf29e13f0
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2014-01-06 04:53:40 +0000

    added Log for import script and separated the log from script

--- web/protected/cronjobs/ImportProduct_Run.php
+++ web/protected/cronjobs/ImportProduct_Run.php
@@ -1,8 +1,12 @@
 <?php
 require_once dirname(__FILE__) . '/../../bootstrap.php';
+
 //checking usage
 if ($argc != 4)
 		die("Usage: ImportProduct siteCode(37,werew,121fd|all) supplierids(1,2,3|all) totalrecords(30|all)\r\n");
+
+if (!Core::getUser() instanceof UserAccount)
+	Core::setUser(BaseServiceAbastract::getInstance('UserAccount')->get(UserAccount::ID_SYSTEM_ACCOUNT));
 
 $libCodes = (($libCodes = trim($argv[1])) === 'all' ? array() : explode(',', str_replace(' ', '', $libCodes)));
 $supplierIds = (($supplierIds = trim($argv[2])) === 'all' ? array() : explode(',', str_replace(' ', '', $supplierIds)));
@@ -13,3 +17,4 @@ ImportProduct::log("== Site Codes: '" . implode("', '", $libCodes), 'load');
 ImportProduct::log("== Supplier IDS: " . implode(', ', $supplierIds), 'load');
 ImportProduct::log("== Total Records: '" . $totalrecords, 'load');
 ImportProduct::log("=============================================================", 'load');
+ImportProduct::run($libCodes, $supplierIds, $totalrecords);
