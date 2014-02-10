3518e735e421252f7261f38974f3e9072fd13dd9
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2014-02-10 04:15:24 +0000

    implementing the SIP2 client

--- core/main/util/Connectors/comms/BmvComSIP2.php
+++ core/main/util/Connectors/comms/BmvComSIP2.php
@@ -1,5 +1,62 @@
 <?php
 class BmvComSIP2
 {
-	
+	/**
+	 * The sip2 object
+	 * 
+	 * @var SIP2
+	 */
+	private $_sip2;
+	/**
+	 * The cache of the bmvcomsip2 objects
+	 * 
+	 * @var array
+	 */
+	private static $_cache;
+	/**
+	 * Getting the BmvComSIP2 script
+	 * 
+	 * @param unknown $host
+	 * @param unknown $port
+	 * @param unknown $patron
+	 * @param unknown $patronPwd
+	 * 
+	 * @return BmvComSIP2
+	 */
+	public static function getSIP($host, $port, $patron, $patronPwd)
+	{
+		$key = md5($host . $port . $patron . $patronPwd);
+		if(!isset(self::$_cache[$key]))
+		{
+			$className = trim(get_called_class());
+			self::$_cache[$key] = new $className($host, $port, $patron, $patronPwd);
+		}
+		return self::$_cache[$key];
+	}
+	/**
+	 * Constructor
+	 * 
+	 * @param string $host
+	 * @param string $port
+	 * @param string $patron
+	 * @param string $patronPwd
+	 * 
+	 */
+	public function __construct($host, $port, $patron, $patronPwd)
+	{
+		$this->_sip2 = new SIP2();
+		$this->_sip2->hostname = $host;
+		$this->_sip2->port = $port;
+		$this->_sip2->patron = $patron;
+		$this->_sip2->patronpwd = $patronPwd;
+	}
+	/**
+	 * connects to a SIP2 server
+	 * 
+	 * @return boolean
+	 */
+	public function connect()
+	{
+		return $this->_sip2->connect();
+	}
 }
--- web/test.php
+++ web/test.php
@@ -1,6 +1,24 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
 <?php
-$wsdl ='https://ebmv.com.au/?soap=webauth.wsdl';
-$client = new SoapClient($wsdl, array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
-$result = $client->authenticate('A3ADC78482897208E84B759E41DD73E9', '37', 'testuser_yl', '2A2877E4DF17AEED392AA42AD36EE5190E1E1DCC');
-echo '<h3>Result came back from '. $wsdl . ':</h3>';
-echo '<textarea style="width: 100%; height: 200px;">' . $result . '</textarea>';
+require_once dirname(__FILE__) . '/bootstrap.php';
+try
+{
+	$host = '206.187.32.61';
+	$port = '8163';
+	$patron = 'BMVCO';
+	$patronPwd = 'YPRLBMV';
+	$result = BmvComSIP2::getSIP($host, $port, $patron, $patronPwd)->connect();
+	var_dump($result);
+}
+catch(Exception $ex)
+{
+	echo '<h3>' . $ex->getMessage() . '</h3>';
+	echo $ex->getTraceAsString();
+}
+?>
+</body>
+</html>
