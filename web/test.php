9e2b6253e0c8253d8210f52238d1290dcac28978
helin16@gmail.com <helin16@gmail.com@ce108b0b-d4c9-daef-1626-9045e9f48059>
2014-02-17 06:06:15 +0000

    testing SIP2 with whitehorse

--- web/test.php
+++ web/test.php
@@ -11,14 +11,24 @@ try
 	$port = '8163';
 	$patron = '20007005832986';
 	$patronPwd = '1234';
+	$sip = new SIP2();
+	$sip->AN = 'BOX';
+	$sip->hostname = $host;
+	$sip->port = $port;
+	$sip->patron = $patron;
+	//send selfcheck status message
+	$in = $sip->msgSCStatus();
+	$result = $sip->parseACSStatusResponse($sip->get_message($in));
+	var_dump($result);
+	
 // 	$patron = '11380047hj';
 // 	$patronPwd = '1234cxzcx';
-	echo '<pre>';
-// 	$result = BmvComSIP2::getSIP(BaseServiceAbastract::getInstance('Library')->get(2), $host, $port, $patron, $patronPwd)
-// 		->login('11380047', '1234');
-	$result = BmvComSIP2::getSIP($host, $port, BaseServiceAbastract::getInstance('Library')->get(4)->getInfo('lib_timezone'))->getPatronInfo($patron, $patronPwd);
-	var_dump($result);
-	echo '</pre>';
+// 	echo '<pre>';
+// // 	$result = BmvComSIP2::getSIP(BaseServiceAbastract::getInstance('Library')->get(2), $host, $port, $patron, $patronPwd)
+// // 		->login('11380047', '1234');
+// 	$result = BmvComSIP2::getSIP($host, $port, BaseServiceAbastract::getInstance('Library')->get(4)->getInfo('lib_timezone'))->getPatronInfo($patron, $patronPwd);
+// 	var_dump($result);
+// 	echo '</pre>';
 }
 catch(Exception $ex)
 {
