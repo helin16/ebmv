1db7ccc84e4bc676796ce3041e281de3e5381efe
l he <helin16@gmail.com>
2014-03-24 20:04:08 +1100

    change soaptest.php

--- web/soaptest.php
+++ web/soaptest.php
@@ -5,14 +5,23 @@
 <body>
 <?php
 require_once dirname(__FILE__) . '/bootstrap.php';
+function resultFunc($funcName, $result)
+{
+	echo '<h3>Result came back from '. $funcName . ':</h3>';
+	echo '<textarea style="width: 100%; height: 200px;">' . $result . '</textarea>';
+	echo '</pre>';
+}
 try
 {
 	$wsdl ='https://ebmv.com.au/?soap=webauth.wsdl';
 	$client = new SoapClient($wsdl, array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
-	$result = $client->authenticate('A3ADC78482897208E84B759E41DD73E9', '37', 'testuser_yl', '2A2877E4DF17AEED392AA42AD36EE5190E1E1DCC');
-	echo '<h3>Result came back from '. $wsdl . ':</h3>';
-	echo '<textarea style="width: 100%; height: 200px;">' . $result . '</textarea>';
-	echo '</pre>';
+	$funcName = "authenticate";
+	$result = $client->$funcName('A3ADC78482897208E84B759E41DD73E9', '37', 'testuser_yl', '2A2877E4DF17AEED392AA42AD36EE5190E1E1DCC');
+	resultFunc($funcName, $result);
+	
+	$funcName = "getUserLocalInfo";
+	$result = $client->getUserLocalInfo('37', 'test_user', sha1('test_pass'));
+	resultFunc($funcName, $result);
 }
 catch(Exception $ex)
 {
