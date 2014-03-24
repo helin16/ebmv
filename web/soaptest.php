6b70857173e0d64809b4c9fc7d41acc653537317
l he <helin16@gmail.com>
2014-03-24 20:07:21 +1100

    disable cache

--- web/soaptest.php
+++ web/soaptest.php
@@ -14,7 +14,7 @@ function resultFunc($funcName, $result)
 try
 {
 	$wsdl ='https://ebmv.com.au/?soap=webauth.wsdl';
-	$client = new SoapClient($wsdl, array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
+	$client = new SoapClient($wsdl, array('exceptions' => true, 'encoding'=>'utf-8', 'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP), 'cache_wsdl'=>WSDL_CACHE_NONE );
 	$funcName = "authenticate";
 	$result = $client->$funcName('A3ADC78482897208E84B759E41DD73E9', '37', 'testuser_yl', '2A2877E4DF17AEED392AA42AD36EE5190E1E1DCC');
 	resultFunc($funcName, $result);
