d996a9681cea9f048242bd27e292b96659fd0593
lhe <lhe@bytecraft.com.au>
2014-07-21 16:44:14 +1000

    added reverse proxy script

--- web/reservseProxy.php
+++ web/reservseProxy.php
@@ -1,16 +1,33 @@
 <?php
-	$url = $_REQUEST ['url'];
-	// to specify http headers like `User-Agent`,
-	// you could create a context like so:
-	$options = array (
-			'http' => array (
-					'method' => "GET",
-					'header' => "User-Agent: PHP\r\nX-PARTNER:ebmv.com.au" 
-			) 
-	);
-	// create context
-	$context = stream_context_create ( $options );
-	// open file with the above http headers
-	$content = file_get_contents ( $url, false, $context );
-	echo $content;
+require 'bootstrap.php';
+$reuqestUrlParams = parse_url ( trim ( $_REQUEST ['url'] ) );
+$proxy = new ProxyHandler ( array (
+		'baseUri' => '/' . basename ( __FILE__ ),
+		'proxyUri' => trim ( $_REQUEST ['url'] ) 
+) );
+$proxy->setCurlOption ( CURLOPT_PROXY, 'proxy.bytecraft.internal:3128' );
+
+ob_start ();
+// Check for a success
+if ($proxy->execute ()) {
+	// print_r($proxy->getCurlInfo()); // Uncomment to see request info
+} else {
+	echo $proxy->getCurlError ();
+}
+$proxy->close ();
+$html = ob_get_contents ();
+ob_end_clean ();
+
+$doc = new DOMDocument ();
+@$doc->loadHTML ( $html ); // supress parsing errors with @
+
+$imgs = $doc->getElementsByTagName ( 'img' );
+foreach ( $imgs as $img ) {
+	$img_src = trim($img->getAttribute ( 'src' ));
+	if (substr($img_src, 0, 1) === '/') {
+		$img->setAttribute ( 'src', $reuqestUrlParams['scheme'] . '://' . $reuqestUrlParams['host'] . $img_src );
+	}
+}
+$html = $doc->saveHTML();
+echo $html;
 ?>
