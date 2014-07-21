09f14e6eb03dd2622954b3fa0dc337e2032b008f
lhe <lhe@bytecraft.com.au>
2014-07-21 17:09:21 +1000

    reverseProxy

--- web/reservseProxy.php
+++ web/reservseProxy.php
@@ -1,41 +1,19 @@
 <?php
-require 'bootstrap.php';
-if(!isset($_REQUEST ['url']) || ($url = trim ( $_REQUEST ['url'] )) === '')
-	die;
-
-$reuqestUrlParams = parse_url ( $url );
-$proxy = new ProxyHandler ( array (
-		'baseUri' => '/' . basename ( __FILE__ ),
-		'proxyUri' => trim ( $_REQUEST ['url'] ) 
-) );
-$proxy->setCurlOption ( CURLOPT_HTTPHEADER, array('X-PARTNER: ebmv.com.au') );
-// $proxy->setCurlOption ( CURLOPT_PROXY, 'proxy.bytecraft.internal:3128' );
-
-ob_start ();
-// Check for a success
-if ($proxy->execute ()) {
-	// print_r($proxy->getCurlInfo()); // Uncomment to see request info
-} else {
-	echo $proxy->getCurlError ();
-}
-$proxy->close ();
-$html = trim(ob_get_contents ());
-ob_end_clean ();
-
-if($html === '') {
-	FrontEndPageAbstract::show404Page('Can NOT requested page', 'Can NOT requested page: ' . $url);
-}
-
-$doc = new DOMDocument ();
-@$doc->loadHTML ( $html ); // supress parsing errors with @
-
-$imgs = $doc->getElementsByTagName ( 'img' );
-foreach ( $imgs as $img ) {
-	$img_src = trim($img->getAttribute ( 'src' ));
-	if (substr($img_src, 0, 1) === '/') {
-		$img->setAttribute ( 'src', $reuqestUrlParams['scheme'] . '://' . $reuqestUrlParams['host'] . $img_src );
-	}
-}
-$html = $doc->saveHTML();
-echo $html;
+if (! isset ( $_REQUEST ['url'] ) || ($url = trim ( $_REQUEST ['url'] )) === '')
+	die ();
+	// to specify http headers like `User-Agent`,
+	// you could create a context like so:
+$options = array (
+		'http' => array (
+				'method' => "GET",
+				'header' => "User-Agent: PHP\r\nX-PARTNER:ebmv.com.au" 
+		) 
+);
+// create context
+$context = stream_context_create ( $options );
+// open file with the above http headers
+$content = file_get_contents ( $url, false, $context );
+$content = preg_replace ( "#(<\s*img\s+[^>]*src\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
+$content = preg_replace ( "#(<\s*a\s+[^>]*href\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
+echo $content;
 ?>
