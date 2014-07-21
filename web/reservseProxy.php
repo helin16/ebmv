fedd75e8123e6bfccd91bb3ede08c7e050f5d9c2
lhe <lhe@bytecraft.com.au>
2014-07-22 09:41:58 +1000

    added caching technique onto reservseProxy

--- web/reservseProxy.php
+++ web/reservseProxy.php
@@ -1,19 +1,39 @@
 <?php
-if (! isset ( $_REQUEST ['url'] ) || ($url = trim ( $_REQUEST ['url'] )) === '')
-	die ();
+function getHTML($pageUrl)
+{
 	// to specify http headers like `User-Agent`,
 	// you could create a context like so:
-$options = array (
-		'http' => array (
-				'method' => "GET",
-				'header' => "User-Agent: PHP\r\nX-PARTNER:ebmv.com.au" 
-		) 
-);
-// create context
-$context = stream_context_create ( $options );
-// open file with the above http headers
-$content = file_get_contents ( $url, false, $context );
-$content = preg_replace ( "#(<\s*img\s+[^>]*src\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
-$content = preg_replace ( "#(<\s*a\s+[^>]*href\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
-echo $content;
+	$options = array (
+			'http' => array (
+					'method' => "GET",
+					'header' => "User-Agent: PHP\r\nX-PARTNER:ebmv.com.au"
+			)
+	);
+	// create context
+	$context = stream_context_create ( $options );
+	// open file with the above http headers
+	$content = file_get_contents ( $pageUrl, false, $context );
+	$content = preg_replace ( "#(<\s*img\s+[^>]*src\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
+	$content = preg_replace ( "#(<\s*a\s+[^>]*href\s*=\s*[\"']\s*)(?!http)([^\"'>]+)(\s*[\"'>]+)#", '$1http://www.chinesecio.com/$2$3', $content );
+	return $content;
+}
+
+if (! isset ( $_REQUEST ['url'] ) || ($url = trim ( $_REQUEST ['url'] )) === '')
+	die ();
+
+$caching = (! isset ( $_REQUEST ['nocaching'] ) || trim ( $_REQUEST ['nocaching'] ) !== '1') ? true : false;
+if($caching === true && extension_loaded('apc') && ini_get('apc.enabled'))
+{
+	$key = md5($url);
+	if(!apc_exists($key))
+	{
+		$html = getHTML($url);
+		apc_add($key, $html);
+	}
+	else
+		$html = apc_fetch($key);
+}
+else
+	$html = getHTML($url);
+echo $html;
 ?>
