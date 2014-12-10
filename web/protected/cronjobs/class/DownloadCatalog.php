72155dfca27a5c932d8974d75f95706eca90a46e
lhe <lhe@bytecraft.com.au>
2014-12-10 14:55:35 +1100

    increase page size for catalog downloading

--- web/protected/cronjobs/class/DownloadCatalog.php
+++ web/protected/cronjobs/class/DownloadCatalog.php
@@ -12,7 +12,7 @@ abstract class DownloadCatalogFromSupplier
 				continue;
 			$script->setDebugMode($debugMode)
 				->setEchoLogging(true)
-				->downloadCatalog(ProductType::get(ProductType::ID_BOOK), 100);
+				->downloadCatalog(ProductType::get(ProductType::ID_BOOK), 10000);
 		}
 	}
 	
