1737f14fb0e00102d65a3a39416a391748c955fb
lhe <lhe@bytecraft.com.au>
2014-12-18 17:07:16 +1100

    again

--- web/protected/cronjobs/class/CleanCategories.php
+++ web/protected/cronjobs/class/CleanCategories.php
@@ -4,15 +4,21 @@ abstract class CleanCategories
 {
 	public static function run()
 	{
+		echo "BEGIN\n";
 		$cateMap = self::_getSameCategoies();
+		echo 'Got ' . count($cateMap) . ") categories to loop:\n";
 		if(count($cateMap) === 0)
 			return;
 		foreach($cateMap as $cateName => $cateArray)
 		{
+			echo "Start process: $cateName\n";
+			echo "No categories to go any more SKIPPING\n";
 			if(count($cateArray) <= 1) //nothing to consider
 				continue;
+			
 			$mergeTo = array_shift($cateArray);
 			$mergeFromIds = array_map(create_function('$a', 'return $a->getId()'), $cateArray);
+			echo "No category to go any more SKIPPING\n";
 			if(count($mergeFromIds) === 0)
 				continue;
 			echo 'Start merging( ' . implode(', ', $mergeFromIds) . ') to ' . $mergeTo->getName() . '(' . $mergeTo->getId() . ")\n";
@@ -21,6 +27,7 @@ abstract class CleanCategories
 			Category::updateByCriteria('active = 0', 'id != ' .$mergeTo->getId() . ' AND id in (' . implode(', ', $mergeFromIds) . ')');
 			echo "END\n";
 		}
+		echo "FINSHED\n";
 	}
 	/**
 	 * Getting the category map
