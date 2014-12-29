0cde9dea89be4a6ef6537ba2dc6536ce0d822fa6
lin <helin16@gmail.com>
2014-12-29 21:02:45 +1100

    changed cronjob script

--- web/protected/cronjobs/importProducts.sh
+++ web/protected/cronjobs/importProducts.sh
@@ -1,2 +1,7 @@
-/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 2,3,4,5,7,9 all > /tmp/productImport_`date +"%d_%b_%y"`.log
+#/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 2 all > /tmp/productImport_`date +"%d_%b_%y"`_YuanLiu.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 3 all > /tmp/productImport_`date +"%d_%b_%y"`_TaiKong.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 4 all > /tmp/productImport_`date +"%d_%b_%y"`_WenHui.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 5 all > /tmp/productImport_`date +"%d_%b_%y"`_XinMinNewsPaper.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 7 all > /tmp/productImport_`date +"%d_%b_%y"`_ZCOM.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 9 all > /tmp/productImport_`date +"%d_%b_%y"`_Apabi.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/AutoReturnExpiredItems.php > /tmp/autoExpiryShelfItems_`date +"%d_%b_%y"`.log
