497e9ce3c3d95e5aaf96fe19065807765f7cded3
lhe <lhe@bytecraft.com.au>
2015-03-13 09:44:46 +1100

    again

--- web/test.php
+++ web/test.php
@@ -1,8 +1,54 @@
 <?php
 require_once 'bootstrap.php';
-require_once dirname(__FILE__) . '/protected/classes/WebUser/WebUserManager.php';
+class test
+{
+	/**
+	 * login for the library reader
+	 *
+	 * @param Library $lib
+	 * @param unknown $username
+	 * @param unknown $password
+	 */
+	public static function login(Library $lib, $libCardNo, $password)
+	{
 
-$userAccount = WebUserManager::login(Library::get(3), '11380047', '1234');
+		if (! Core::getUser () instanceof UserAccount)
+			Core::setUser ( UserAccount::get ( UserAccount::ID_SYSTEM_ACCOUNT ) );
+		if(self::$fromLocalDB === true)
+		{
+			$userAccount = UserAccount::getUserByUsernameAndPassword($libCardNo, $password, $lib);
+			// check whether the library has the user or not
+			if (! $userAccount instanceof UserAccount)
+				throw new CoreException ( 'Invalid login please contact ebmv admin!' );
+		}
+		else
+		{
+			// check whether the library has the user or not
+			if (! LibraryConnectorAbstract::getScript ($lib)->chkUser ( $libCardNo, $password ))
+				throw new CoreException ( 'Invalid login please contact your library!' );
+
+			// get the information from the library system
+			$userInfo = LibraryConnectorAbstract::getScript ($lib)->getUserInfo ( $libCardNo, $password );
+			// check whether our local db has the record already
+			if (($userAccount = UserAccount::getUserByUsername ( $libCardNo, $lib )) instanceof UserAccount) {
+				$person = $userAccount->getPerson();
+				$userAccount = UserAccount::updateUser ( $userAccount, $lib, $userInfo->getUsername (), $userInfo->getPassword (), null, Person::createNudpatePerson( $userInfo->getFirstName (), $userInfo->getLastName(), $person ) );
+			} else {		// we need to create a user account from blank
+				$userAccount = UserAccount::createUser ( $lib, $userInfo->getUsername (), $userInfo->getPassword (), Role::get(Role::ID_READER), Person::createNudpatePerson( $userInfo->getFirstName (), $userInfo->getLastName() ) );
+			}
+		}
+
+		$role = null;
+		if (! Core::getRole () instanceof Role)
+		{
+			if (count ( $roles = $userAccount->getRoles () ) > 0)
+				$role = $roles [0];
+		}
+		Core::setUser($userAccount, $role);
+		return $userAccount;
+	}
+}
+$userAccount = test::login(Library::get(3), '11380047', '1234');
 var_dump($userAccount);
 
 ?>
