a7cca891e1963b460c37a311219292ab5b5aea14
lhe <lhe@bytecraft.com.au>
2015-03-13 09:42:29 +1100

    again

--- web/test.php
+++ web/test.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'bootstrap.php';
-require_once dirname(__FILE__) . '/protected/classes/Webuser/WebUserManager.php';
+require_once dirname(__FILE__) . '/protected/classes/WebUser/WebUserManager.php';
 
 $userAccount = WebUserManager::login(Library::get(3), '11380047', '1234');
 var_dump($userAccount);
