30e5b4890e33e5f0960deb210777b2df313190cc
lhe <lhe@bytecraft.com.au>
2015-06-02 12:54:24 +1000

    removed ZCOM

--- web/protected/cronjobs/importProducts.sh
+++ web/protected/cronjobs/importProducts.sh
@@ -2,6 +2,5 @@
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 3 all > /tmp/productImport_`date +"%d_%b_%y"`_TaiKong.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 4 all > /tmp/productImport_`date +"%d_%b_%y"`_WenHui.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 5 all > /tmp/productImport_`date +"%d_%b_%y"`_XinMinNewsPaper.log
-/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 7 all > /tmp/productImport_`date +"%d_%b_%y"`_ZCOM.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 9 all > /tmp/productImport_`date +"%d_%b_%y"`_Apabi.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/AutoReturnExpiredItems.php > /tmp/autoExpiryShelfItems_`date +"%d_%b_%y"`.log
