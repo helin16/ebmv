7563e8d9dfb57bc4cc1160a52d116d4524d6e2e0
lhe <lhe@bytecraft.com.au>
2015-07-16 16:47:27 +1000

    added google analytics code for each library

--- web/protected/controls/GoogleAnalytics/GoogleAnalytics.php
+++ web/protected/controls/GoogleAnalytics/GoogleAnalytics.php
@@ -19,9 +19,12 @@ class GoogleAnalytics extends TClientScript
 	 */
 	private function _getJs()
 	{
-		$js = '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document, "script","//www.google-analytics.com/analytics.js","ga");';
-		$js .= 'ga("create", "UA-65211844-1", "auto");';
-		$js .= 'ga("send", "pageview");';
+		$js = '';
+		if(Core::getLibrary() instanceof Library && ($analyticsTrackingCode = trim(Core::getLibrary()->getInfo('analytics_track_code'))) !== '') {
+			$js .= '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document, "script","//www.google-analytics.com/analytics.js","ga");';
+			$js .= 'ga("create", "' . $analyticsTrackingCode . '", "auto");';
+			$js .= 'ga("send", "pageview");';
+		}
 		return $js;
 	}
 }
