44f781f9b5af3f3ebdfbff1e2abca456c3e7c398
lhe <lhe@bytecraft.com.au>
2015-07-28 16:27:59 +1000

    added DLTX script

--- core/main/util/Connectors/SupplierConnector/SC_DLTX.php
+++ core/main/util/Connectors/SupplierConnector/SC_DLTX.php
@@ -20,7 +20,7 @@ class SC_DLTX extends SupplierConnectorAbstract implements SupplierConn
 	 *
 	 * @return SimpleXMLElement
 	 */
-	private function _getJsonFromUrl($url, $pageNo = 1)
+	private function _getJsonFromUrl($url, ProductType $type, $pageNo = 1)
 	{
 		$url = str_replace('{page_no}', $pageNo, $url);
 		if($this->_debugMode === true)
@@ -35,10 +35,13 @@ class SC_DLTX extends SupplierConnectorAbstract implements SupplierConn
 		{
 			if(!isset(self::$cache['data']))
 				self::$cache['data'] = array();
-			self::$cache['data'] = array_merge(self::$cache['data'], $result['data']);
+			foreach($result['data'] as $row)
+			{
+				self::$cache['data'][] = $this->_fakeProduct($type, $row);
+			}
 		}
 		if(count(self::$cache['data']) < intval($result['total']) )
-			$this->_getJsonFromUrl($url, $pageNo + 1);
+			$this->_getJsonFromUrl($url, $type, $pageNo + 1);
 		return $this;
 	}
 	/**
@@ -50,16 +53,15 @@ class SC_DLTX extends SupplierConnectorAbstract implements SupplierConn
 	public function getProductListInfo(ProductType $type = null)
 	{
 		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, 'Getting product list info:', __FUNCTION__);
-// 		$importUrl =trim($this->_supplier->getInfo('import_url'));
 		$importUrl = 'http://public.dooland.com/v1/Magazine/lists/page/{page_no}';
 
 		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, '::got import url:' . $importUrl, __FUNCTION__);
-		$this->_getJsonFromUrl($importUrl);
+		if(!isset(self::$cache['data']))
+			$this->_getJsonFromUrl($importUrl, $type);
 
 		if(count(self::$cache['data']) === 0)
 			throw new SupplierConnectorException('Can NOT any data information from ' . $importUrl . '!');
-		$array = self::$cache['data'];
-// 		$array = SupplierConnectorProduct::getInitPagination($xml);
+		$array = SupplierConnectorProduct::getInitPagination ( null, count(self::$cache['data']), 1, DaoQuery::DEFAUTL_PAGE_SIZE );
 		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, '::got array from results:' . print_r($array, true) , __FUNCTION__);
 		return $array;
 	}
@@ -70,66 +72,50 @@ class SC_DLTX extends SupplierConnectorAbstract implements SupplierConn
 	public function getProductList($pageNo = 1, $pageSize = DaoQuery::DEFAUTL_PAGE_SIZE, ProductType $type = null, $onceOnly = false)
 	{
 		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, 'Getting product list:', __FUNCTION__);
-		if(trim($pageSize) === '')
-		{
-			$pageInfo = $this->getProductListInfo($type);
-			$pageSize = $pageInfo['totalRecords'];
-			if($this->_debugMode === true) SupplierConnectorAbstract::log($this, '::pageInfo:' . print_r($pageInfo, true), __FUNCTION__);
-		}
-
-		$array = array();
-		$params = array('type'=>'new', 'pagesize' => $pageSize, 'page' => $pageNo, 'siteid' => trim($this->_lib->getInfo('aus_code')));
-		$importUrl =trim($this->_supplier->getInfo('import_url'));
-		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, '::got import url:' . $importUrl, __FUNCTION__);
-
-		$xml = $this->_getXmlFromUrl($importUrl, 'new', $pageNo, $pageSize, $type);
-		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, '::got results:' . (!$xml instanceof SimpleXMLElement ? trim($xml) : $xml->asXML()) , __FUNCTION__);
-		foreach($xml->children() as $childXml)
-		{
-			$array[] = $childXml;
-		}
-
-		if($onceOnly === false)
-		{
-			//next page
-			$attributes = $xml->attributes();
-			if(isset($attributes['totalPages']) && $pageNo < $attributes['totalPages'])
-				$array = array_merge($array, $this->getProductList($pageNo + 1, $pageSize, $type));
-		}
-		return $array;
+		if(!isset(self::$cache['data']))
+			$this->_getJsonFromUrl($importUrl, $type);
+		return array_slice(self::$cache['data'], ($pageNo - 1) * $pageSize, $pageSize);
 	}
 	/**
 	 * (non-PHPdoc)
+	 *
 	 * @see SupplierConn::getOnlineReadUrl()
 	 */
-	public function getOnlineReadUrl(Product $product, UserAccount $user)
+	public function getOnlineReadUrl(Product $product, UserAccount $user) {
+// 		$url = explode (',', $this->_supplier->getInfo ( 'view_url' ) );
+		$content = $this->_getContent($product);
+		$url = trim('http://public.dooland.com/v1/Read/webonline/?content=' . $content);
+		if ($url === false || count ( $url ) === 0)
+			throw new SupplierConnectorException ( 'Invalid view url for supplier: ' . $this->_supplier->getName () );
+		return $url;
+	}
+	/**
+	 * Getting the encrypted content string
+	 *
+	 * @param Product $product
+	 *
+	 * @return string
+	 */
+	private function _getContent(Product $product)
 	{
-		$readurl = $this->_supplier->getInfo('view_url');
-		if($readurl === false || count($readurl) === 0)
-			throw new SupplierConnectorException('Invalid view url for supplier: ' . $this->_supplier->getName());
-		$readurl = str_replace('{cno}', $product->getAttribute('cno'), $readurl);
-		return $readurl;
+		$contentArr = array(
+			'appid' => self::APP_ID,
+			'id' => $product->getAttribute ( 'isbn' ),
+			'issuetype' => 'mag',
+			'pageid' => '1',
+			'accountid' => '111',
+			'timestamp' => time()
+		);
+		return urlencode($this->_encrypt(json_encode($contentArr), self::APP_SECRET));
 	}
 	/**
 	 * (non-PHPdoc)
+	 *
 	 * @see SupplierConn::getProduct()
 	 */
-	public function getProduct(Product $product)
-	{
-		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, 'Getting Product from supplier:', __FUNCTION__);
-
-		$params = array("siteid" => trim($this->_lib->getInfo('aus_code')),
-				'type' => 'one',
-				'magid' =>  trim($product->getAttribute('cno')),
-				'isbn' => trim($product->getAttribute('isbn'))
-		);
-		$url = $this->_supplier->getInfo('import_url') . '?' . http_build_query($params);
-		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, 'Sending params to :' . $url, __FUNCTION__);
-
-		$results = SupplierConnectorAbstract::readUrl($url, BmvComScriptCURL::CURL_TIMEOUT);
-		if($this->_debugMode === true) SupplierConnectorAbstract::log($this, 'Got results:' . print_r($results, true), __FUNCTION__);
-
-		return SupplierConnectorProduct::getProduct(new SimpleXMLElement($results));
+	public function getProduct(Product $product) {
+		$pro = SupplierConnectorProduct::getProduct ( $this->_fakeProduct ( $product->getProductType(), null, $product ) );
+		return $pro;
 	}
 	/**
 	 * (non-PHPdoc)
@@ -197,30 +183,60 @@ class SC_DLTX extends SupplierConnectorAbstract implements SupplierConn
 // 		$productKey = $product instanceof Product ? $product->getAttribute ( 'cno' ) : $this->_getProductKey($date);
 // 		if (($coverImg = $this->_getCoverImage ( $productKey )) !== '')
 // 			$readOnlineCopy = 1;
-
 		$xml = new SimpleXMLElement ( '<' . $type->getName () . '/>' );
 		$xml->BookName = $product instanceof Product ? $product->getTitle () : $data['title'];
-		$xml->Isbn = $product instanceof Product ? $product->getAttribute ( 'isbn' ) : '';
-		$xml->NO = $product instanceof Product ? $product->getAttribute ( 'cno' ) : $data['magazineId'];
+		$xml->Isbn = $product instanceof Product ? $product->getAttribute ( 'isbn' ) : $data['magazineId'];
+		$xml->NO = $product instanceof Product ? $product->getAttribute ( 'cno' ) : $data['pid'];
 		$xml->Author = $product instanceof Product ? $product->getAttribute ( 'author' ) : $this->_supplier->getName ();
 		$xml->Press = $product instanceof Product ? $product->getAttribute ( 'publisher' ) : $this->_supplier->getName ();
 		$xml->PublicationDate = $product instanceof Product ? $product->getAttribute ( 'publish_date' ) : $data['pubDate'];
 		$xml->Words = '';
-		$xml->FrontCover = $coverImg;
-		$xml->Introduction = $product instanceof Product ? $product->getAttribute ( 'description' ) : $this->_supplier->getName () . ': ' . $date->format ( 'd F Y' );
+		$xml->FrontCover = $product instanceof Product ? $product->getAttribute ( 'image' ) : $data['thumbnail'];
+		$xml->Introduction = $product instanceof Product ? $product->getAttribute ( 'description' ) : $data['des'];
 		$xml->Cip = '';
 		$xml->SiteID = trim ( $this->_lib->getInfo ( 'aus_code' ) );
-		$xml->Language = trim($this->_getLanguageCode());
+		$xml->Language = 'zh-CN';
 
 		$publishDate = new UDate ( $xml->PublicationDate );
-		$xml->BookType = ($bookName = trim($this->_supplier->getName())) . '/' . ($bookName . $publishDate->format ('Y')) . '/' . ($bookName . $publishDate->format('m'));
+		$xml->BookType = ($bookName = trim($xml->BookName)) . '/' . ($bookName . $publishDate->format ('Y')) . '/' . ($bookName . $publishDate->format('m'));
 		$copiesXml = $xml->addChild( 'Copies' );
 		$readOnline = $copiesXml->addChild ($this->_getLibOwnsType ( LibraryOwnsType::ID_ONLINE_VIEW_COPIES )->getCode ());
-		$readOnline->Available = $readOnlineCopy;
+		$readOnline->Available = 1;
 		$readOnline->Total = 1;
 		$download = $copiesXml->addChild ( $this->_getLibOwnsType ( LibraryOwnsType::ID_DOWNLOAD_COPIES )->getCode () );
 		$download->Available = $readOnlineCopy;
 		$download->Total = 1;
 		return $xml;
 	}
+	/**
+	 * Getting the library owns type
+	 *
+	 * @param unknown $typeId
+	 *
+	 * @return LibraryOwnsType
+	 */
+	private function _getLibOwnsType($typeId) {
+		if (! isset ( self::$cache ['libType'] ))
+			self::$cache ['libType'] = array ();
+
+		if (! isset ( self::$cache ['libType'] [$typeId] ))
+			self::$cache ['libType'] [$typeId] = LibraryOwnsType::get ( $typeId );
+
+		return self::$cache ['libType'] [$typeId];
+	}
+
+	private function _encrypt($input, $key) {
+		$size = mcrypt_get_block_size ( 'des', 'ecb' );
+		$pad = $size - (strlen ( $input ) % $size);
+		$input = $input . str_repeat ( chr ( $pad ), $pad );
+		$td = mcrypt_module_open ( 'des', '', 'ecb', '' );
+		$iv = @mcrypt_create_iv ( mcrypt_enc_get_iv_size ( $td ), MCRYPT_RAND );
+		@mcrypt_generic_init ( $td, $key, $iv );
+		$data = mcrypt_generic ( $td, $input );
+		mcrypt_generic_deinit ( $td );
+		mcrypt_module_close ( $td );
+		$data = base64_encode ( $data );
+		return $data;
+	}
+
 }
--- web/protected/pages/FrontEnd/ProductDetails/ProductDetailsController.php
+++ web/protected/pages/FrontEnd/ProductDetails/ProductDetailsController.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * This is the product details page
- * 
+ *
  * @package    Web
  * @subpackage Controller
  * @author     lhe<helin16@gmail.com>
  */
-class ProductDetailsController extends FrontEndPageAbstract  
+class ProductDetailsController extends FrontEndPageAbstract
 {
 	/**
 	 * @var Product
@@ -21,7 +21,7 @@ class ProductDetailsController extends FrontEndPageAbstract
 		if(isset($this->Request['id']))
 			$this->_product = Product::get($this->Request['id']);
 	}
-	
+
 	/**
      * (non-PHPdoc)
      * @see TControl::onLoad()
@@ -68,11 +68,11 @@ class ProductDetailsController extends FrontEndPageAbstract
 		);
 		return $array;
 	}
-	
+
 	public function getUrl($sender, $params)
 	{
 		$errors = $results = array();
-        try 
+        try
         {
         	if(!$this->_product->getSupplier() instanceof Supplier)
         		throw new Exception('System Error: no supplier found for this book!');
@@ -99,10 +99,10 @@ class ProductDetailsController extends FrontEndPageAbstract
         	try
         	{
         		SupplierConnectorAbstract::getInstance($this->_product->getSupplier(), Core::getLibrary())->borrowProduct($this->_product, Core::getUser());
-        		ProductShelfItem::borrowItem(Core::getUser(), $this->_product, Core::getLibrary()); 
+        		ProductShelfItem::borrowItem(Core::getUser(), $this->_product, Core::getLibrary());
         		//increasing statics
         		$this->_product->addStatic(Core::getLibrary(), ProductStaticsType::get(ProductStaticsType::ID_BORROW_RATE), 1);
-        	} 
+        	}
         	catch (SupplierConnectorException $e)
         	{
         		$results['warning'] = 'Failed to borrow this item from supplier';
@@ -119,28 +119,28 @@ class ProductDetailsController extends FrontEndPageAbstract
         }
         $params->ResponseData = StringUtilsAbstract::getJson($results, $errors);
 	}
-	
+
 	public function updateProduct($sender, $params)
 	{
 		$errors = $results = array();
-        try 
+        try
         {
         	if(!($supplier = $this->_product->getSupplier()) instanceof Supplier)
         		throw new Exception('System Error: no supplier found for this book!');
-        	
-        	
+
+
         	//get the borrow limitation
         	$libraryMaxCount = trim(Core::getLibrary()->getInfo('borrow_limit'));
         	$maxBorrowCount = !is_numeric($libraryMaxCount) ? intval(SystemSettings::getSettings(SystemSettings::TYPE_DEFAULT_BORROW_LIMIT)) : intval($libraryMaxCount);
-        	
+
         	//get the user's actual borrowed count
         	if(!($user = Core::getUser()) instanceof UserAccount)
         		Core::setUser(UserAccount::get(UserAccount::ID_GUEST_ACCOUNT));
         	$totalBorrowedNo = intval(Core::getUser()->countBookShelfItem());
-        	
+
         	//increasing statics
         	$this->_product->addStatic(Core::getLibrary(), ProductStaticsType::get(ProductStaticsType::ID_CLICK_RATE), 1);
-        	
+
         	$results['urls'] = array('viewUrl' => false, 'downloadUrl' => false);
         	$results['copies'] = array();
         	if ($totalBorrowedNo >= $maxBorrowCount) //full
@@ -149,7 +149,7 @@ class ProductDetailsController extends FrontEndPageAbstract
         			'zh_CN' => '<a href="/user.html"  title="点击进入您的书架">您的书架</a>已满，请归还一些，然后再继续',
         			'zh_TW' => '<a href="/user.html" title="點擊進入您的書架">您的書架</a>已滿，請歸還一些，然後再繼續'
         		);
-        	else if ($totalBorrowedNo >= $maxBorrowCount * 0.9) 
+        	else if ($totalBorrowedNo >= $maxBorrowCount * 0.9)
         	{
         		$results['warningMsg'] = array(
         			'en' => '<a href="/user.html" title="Go to yourbookshelf">Your bookshelf</a> is almost full.',
@@ -158,15 +158,19 @@ class ProductDetailsController extends FrontEndPageAbstract
         		);
         		SupplierConnectorAbstract::getInstance($this->_product->getSupplier(), Core::getLibrary())->updateProduct($this->_product);
         		$results['urls'] = array('viewUrl' => (trim($supplier->getInfo('view_url')) !== ''), 'downloadUrl' => (trim($supplier->getInfo('download_url')) !== ''));
-        		foreach($this->_product->getLibOwns() as $owns)
+        		foreach(LibraryOwns::getAllByCriteria('libraryId = ? and productId = ?', array(Core::getLibrary()->getId(), $this->_product->getId())) as $owns)
+        		{
         			$results['copies'][$owns->getType()->getId()] = $owns->getJson();
+        		}
         	}
         	else
         	{
 	        	SupplierConnectorAbstract::getInstance($this->_product->getSupplier(), Core::getLibrary())->updateProduct($this->_product);
 	        	$results['urls'] = array('viewUrl' => (trim($supplier->getInfo('view_url')) !== ''), 'downloadUrl' => (trim($supplier->getInfo('download_url')) !== ''));
-	        	foreach($this->_product->getLibOwns() as $owns)
-	        		$results['copies'][$owns->getType()->getId()] = $owns->getJson();
+	        	foreach(LibraryOwns::getAllByCriteria('libraryId = ? and productId = ?', array(Core::getLibrary()->getId(), $this->_product->getId())) as $owns)
+        		{
+        			$results['copies'][$owns->getType()->getId()] = $owns->getJson();
+        		}
         	}
         }
         catch(Exception $ex)
