646101ddc7d09d7b868a58e4b8a41c100790afc2
helin16 <helin16@192.168.200.3>
2015-07-28 23:42:06 +1000

    updated import scripts

--- web/protected/cronjobs/importProducts.sh
+++ web/protected/cronjobs/importProducts.sh
@@ -3,4 +3,5 @@
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 4 all > /tmp/productImport_`date +"%d_%b_%y"`_WenHui.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 5 all > /tmp/productImport_`date +"%d_%b_%y"`_XinMinNewsPaper.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 9 all > /tmp/productImport_`date +"%d_%b_%y"`_Apabi.log
+/usr/bin/php /var/www/ebmv/web/protected/cronjobs/ImportProduct_Run.php all 12 all > /tmp/productImport_`date +"%d_%b_%y"`_DLTX.log
 /usr/bin/php /var/www/ebmv/web/protected/cronjobs/AutoReturnExpiredItems.php > /tmp/autoExpiryShelfItems_`date +"%d_%b_%y"`.log
