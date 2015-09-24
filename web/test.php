3f85875171f9fc6a9a0dbe092a4dc3493f2f494b
helin16 <helin16@192.168.200.213>
2015-09-25 00:00:43 +1000

    test

--- web/test.php
+++ web/test.php
@@ -1,25 +1,8 @@
 <?php
-$options = array(
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_CONNECTTIMEOUT => 0,
-		CURLOPT_URL     => 'http://public.dooland.com/v1/Magazine/lists/page/1',
-		CURLOPT_CUSTOMREQUEST => 'POST'
-// 		,CURLOPT_PROXY   => 'proxy.bytecraft.internal:3128'
-);
-$data = array('osType' => 'Linux', 'appid' => 'f239776c84236b30c83e86993666cc99');
-// $options[CURLOPT_POST] = count($data);
-$data = json_encode($data);
-$options[CURLOPT_POSTFIELDS] = $data;
-var_dump($data);
+require_once 'bootstrap.php';
+$product = Product::get(18621);
 
-$ch = curl_init();
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-// 	'Content-Type: application/json',
-// 	'Content-Length: ' . strlen($data))
-// );
-curl_setopt_array($ch, $options);
-$result = curl_exec($ch);
-curl_close($ch);
+$result = SupplierConnectorAbstract::getInstance($product->getSupplier(), Library::get(10))->updateProduct($product);
 
 var_dump($result);
 
