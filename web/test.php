3580ae003ea348c02b0fe45a511357eda02f7e6a
helin16 <helin16@192.168.200.202>
2015-10-26 19:42:01 +1100

    debug code

--- web/test.php
+++ web/test.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'bootstrap.php';
 
-$result = SupplierConnectorAbstract::getInstance(Supplier::get(12), Library::get(10))->getProductListInfo(ProductType::get(3));
+$result = SupplierConnectorAbstract::getInstance(Supplier::get(12), Library::get(10))->getProductList(1, 10, ProductType::get(3), true);
 
 var_dump($result);
 
