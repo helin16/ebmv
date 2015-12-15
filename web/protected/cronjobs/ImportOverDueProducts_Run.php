93e28e28ec032dc2f5b72fbdb93a586ba80aa5cd
helin16 <helin16@helin16-VirtualBox>
2015-12-15 15:13:27 +1100

    added import over due product script

---
+++ web/protected/cronjobs/ImportOverDueProducts_Run.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__) . '/../../bootstrap.php';
+
+abstract class ImportOverDueIssues
+{
+  public static function run() {
+    if (!Core::getUser() instanceof UserAccount)
+      Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
+    foreach (Library::getAll() as $library) {
+      SupplierConnectorAbstract::getInstance(Supplier::get(12), $library)->getOverDueIssues(ProductType::get(3));
+    }
+  }
+}
+ImportOverDueIssues::run();
--- web/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'bootstrap.php';
-
-$result = SupplierConnectorAbstract::getInstance(Supplier::get(12), Library::get(10))->getProductList(1, 10, ProductType::get(3), true);
-
-var_dump($result);
-
-?>
