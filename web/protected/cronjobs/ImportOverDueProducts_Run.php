77c58fd6817c5114e8fdab5a196eedece8ce766c
helin16 <helin16@helin16-VirtualBox>
2015-12-15 15:15:46 +1100

    added debug code

--- web/protected/cronjobs/ImportOverDueProducts_Run.php
+++ web/protected/cronjobs/ImportOverDueProducts_Run.php
@@ -7,7 +7,7 @@ abstract class ImportOverDueIssues
     if (!Core::getUser() instanceof UserAccount)
       Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
     foreach (Library::getAll() as $library) {
-      SupplierConnectorAbstract::getInstance(Supplier::get(12), $library)->getOverDueIssues(ProductType::get(3));
+      SupplierConnectorAbstract::getInstance(Supplier::get(12), $library)->setEchoLogging(true)->getOverDueIssues(ProductType::get(3));
     }
   }
 }
