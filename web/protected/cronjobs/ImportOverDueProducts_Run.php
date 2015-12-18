d6afc0c0ef1088d7eb4333d491d333934d1f8a16
helin16 <helin16@192.168.200.205>
2015-12-18 23:01:37 +1100

    add memory issue

--- web/protected/cronjobs/ImportOverDueProducts_Run.php
+++ web/protected/cronjobs/ImportOverDueProducts_Run.php
@@ -3,12 +3,10 @@ require_once dirname(__FILE__) . '/../../bootstrap.php';
 
 abstract class ImportOverDueIssues
 {
-  public static function run() {
+  public static function run($libraryId) {
     if (!Core::getUser() instanceof UserAccount)
       Core::setUser(UserAccount::get(UserAccount::ID_SYSTEM_ACCOUNT));
-    foreach (Library::getAll() as $library) {
-      SupplierConnectorAbstract::getInstance(Supplier::get(12), $library)->setDebugMode(true)->setEchoLogging(true)->getOverDueIssues(ProductType::get(3), '2015-08-01');
-    }
+     SupplierConnectorAbstract::getInstance(Supplier::get(12), Library::get($libraryId))->setDebugMode(true)->setEchoLogging(true)->getOverDueIssues(ProductType::get(3), new UDate('2015-08-01'));
   }
 }
-ImportOverDueIssues::run();
+ImportOverDueIssues::run($argv[2]);
